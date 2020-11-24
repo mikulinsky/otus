52947eeb3a37ef65e099a7e26aa624ee5f5f4072
amikulinskii <mikulinskiiav@nspk.ru>
2020-11-25 01:05:16 +0700

    done hw05

---
+++ hw05-autoLog/src/main/java/otus/hw05/autoLog/annotations/Log.java
@@ -0,0 +1,8 @@
+package otus.hw05.autoLog.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Log {
+}
