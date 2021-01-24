e9cdfd8230cdc7d6b32d9883926f45db9756089e
amikulinskii <mikulinskiiav@nspk.ru>
2021-01-24 13:40:18 +0700

    change hashmap key to ID

--- hw07-todo/src/main/java/otus/hw07/todo/listener/homework/HistoryListener.java
+++ hw07-todo/src/main/java/otus/hw07/todo/listener/homework/HistoryListener.java
@@ -3,17 +3,18 @@ package otus.hw07.todo.listener.homework;
 import otus.hw07.todo.listener.Listener;
 import otus.hw07.todo.model.Message;
 
-import java.time.LocalDateTime;
 import java.util.AbstractMap;
 import java.util.HashMap;
 
 public class HistoryListener implements Listener {
 
-    private final HashMap<LocalDateTime, AbstractMap.SimpleEntry<Message, Message>> history = new HashMap<>();
+    private final HashMap<Long, AbstractMap.SimpleEntry<Message, Message>> history = new HashMap<>();
+    private Long nextID = Integer.toUnsignedLong(0);
 
     @Override
     public void onUpdated(Message oldMsg, Message newMsg) {
-        history.put(LocalDateTime.now(), new AbstractMap.SimpleEntry<>(oldMsg, newMsg));
+        history.put(nextID, new AbstractMap.SimpleEntry<>(oldMsg.copy(), newMsg.copy()));
+        nextID++;
     }
 
     public void getHistory() {
