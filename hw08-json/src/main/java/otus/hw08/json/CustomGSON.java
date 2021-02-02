8bf6534b7a365b07b1b77fa11c7a4b3fb510f49d
amikulinskii <mikulinskiiav@nspk.ru>
2021-02-02 13:55:46 +0700

    migrate to custom json object

--- hw08-json/src/main/java/otus/hw08/json/CustomGSON.java
+++ hw08-json/src/main/java/otus/hw08/json/CustomGSON.java
@@ -1,13 +1,11 @@
 package otus.hw08.json;
 
-import org.json.JSONObject;
-
 import java.lang.reflect.Field;
 
 public class CustomGSON {
 
     public static String toJson(Object obj) throws IllegalAccessException {
-        JSONObject jsonObject = new JSONObject();
+        CustomJSONObject jsonObject = new CustomJSONObject();
         Class clazz = obj.getClass();
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
